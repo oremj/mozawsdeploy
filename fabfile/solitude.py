5c984bcd0f136632b6be143c85f2cd12a39ee918
Jason Thomas <jason@lithiumfox.com>
2012-12-31 14:18:50 -0500

    fix error, change security group name

--- fabfile/solitude.py
+++ fabfile/solitude.py
@@ -9,7 +9,6 @@ AMAZON_AMI = 'ami-2a31bf1a'
 
 create_server = partial(ec2.create_server, app='solitude')
 
-
 @task
 def create_web(env, instance_type='m1.small'):
     """
@@ -40,9 +39,9 @@ def create_database(env, password, instance_type='db.m1.small'):
     args: env, password, instance_type
     This function will create the master rw database for solitude.
     """
-    rds_id = "solitude-%s-master" % env
+    rds_id = "solitude-master-%s" % env
     db_name = "solitude_%s" % env
     username = db_name
-    create_rds(rds_id, db_name, username, password, server_type=instance_type,
-                    param_group='solitude-mysql55',
-                    security_groups=['solitude-%s-write' % env])
+    rds.create_rds(rds_id, db_name, username, password, server_type=instance_type,
+                   param_group='solitude-mysql55',
+                   security_groups=['solitude-db-write-%s' % env])
