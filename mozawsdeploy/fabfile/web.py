315de57ae6b405ded7727f4e0669afb37b652373
Jeremiah Orem <oremj@oremj.com>
2013-01-23 00:24:39 -0800

    Split build release up a bit.

--- mozawsdeploy/fabfile/web.py
+++ mozawsdeploy/fabfile/web.py
@@ -1,10 +1,33 @@
 import os
 import re
-import shutil
 import time
-from datetime import datetime
 
 from fabric.api import local, lcd
+
+
+def create_virtualenv(release_dir):
+    local('virtualenv --distribute --never-download %s/venv' % release_dir)
+
+
+def pip_install(release_dir, requirements):
+    local('%s/venv/bin/pip install --exists-action=w --no-deps '
+          '--no-index --download-cache=/tmp/pip-cache '
+          '-f https://pyrepo.addons.mozilla.org '
+          '-r %s' %
+          (release_dir, requirements))
+
+
+def relocatable_virtualenv(release_dir):
+    local('%s/venv/bin/python /usr/bin/virtualenv --relocatable %s/venv' %
+          (release_dir, release_dir))
+
+
+def install_requirements(release_dir, requirements):
+    create_virtualenv(release_dir)
+    pip_install(release_dir, requirements)
+    local('rm -f %s/venv/lib/python2.6/no-global-site-packages.txt' %
+          release_dir)
+    relocatable_virtualenv(release_dir)
 
 
 def build_release(app, project_dir, repo, ref, requirements, settings_dir,
@@ -26,24 +49,16 @@ def build_release(app, project_dir, repo, ref, requirements, settings_dir,
     else:
         release_dir = os.path.join(project_dir, 'builds', release_id)
 
-    requirements = os.path.join(release_dir, app, requirements)
-    settings_dir = os.path.join(release_dir, app, settings_dir)
+    app_dir = os.path.join(release_dir, app)
+    requirements = os.path.join(app_dir, requirements)
+    settings_dir = os.path.join(app_dir, settings_dir)
 
     local('mkdir -p %s' % release_dir)
-    local('git clone %s %s/%s' % (repo, release_dir, app))
-    with lcd('%s/%s' % (release_dir, app)):
+    local('git clone %s %s' % (repo, app_dir))
+    with lcd(app_dir):
         local('git reset --hard %s' % ref)
 
-    local('virtualenv --distribute --never-download %s/venv' % release_dir)
-    local('%s/venv/bin/pip install --exists-action=w --no-deps '
-          '--no-index --download-cache=/tmp/pip-cache '
-          '-f https://pyrepo.addons.mozilla.org '
-          '-r %s' %
-          (release_dir, requirements))
-    local('rm -f %s/venv/lib/python2.6/no-global-site-packages.txt' %
-          release_dir)
-    local('%s/venv/bin/python /usr/bin/virtualenv --relocatable %s/venv' %
-          (release_dir, release_dir))
+    install_requirements(release_dir, requirements)
 
     local('rsync -av %s/settings/ %s/' % (project_dir, settings_dir))
 
@@ -51,25 +66,3 @@ def build_release(app, project_dir, repo, ref, requirements, settings_dir,
         extra(release_dir)
 
     return release_id
-
-
-def remove_old_releases(project_dir, keep=4):
-    """args: keep (default 4) must be at least 2"""
-
-    assert keep >= 2
-
-    build_dir = os.path.join(project_dir, 'builds')
-
-    builds = []
-    for d in os.listdir(build_dir):
-        app, timestamp, ref = d.split('-')
-        dt = datetime.fromtimestamp(int(timestamp))
-        builds.append((dt, os.path.join(build_dir, d)))
-
-    removed = 0
-    builds.sort()
-    for b, d in builds[:-keep]:
-        shutil.rmtree(d)
-        removed += 1
-
-    print "Removed %d old releases." % removed
