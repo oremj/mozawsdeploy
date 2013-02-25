cdc3c85ffa8b73fd3e99adf1dcba5ba6f861c5ae
Jeremiah Orem <oremj@oremj.com>
2013-02-25 15:39:10 -0800

    Add some params to the classifier.

--- mozawsdeploy/puppet.py
+++ mozawsdeploy/puppet.py
@@ -3,13 +3,19 @@ import yaml
 from .ec2 import get_instance
 
 
+def escape(word):
+    return word.encode('ascii', 'ignore')
+
+
 def create_node_yaml(fqdn):
     i = get_instance(instance_ids=[fqdn])
     if not i:
         return ''
     tclass = '%s-private::%s::%s' % (i.tags['App'],
                                      i.tags['Type'], i.tags['Env'])
-    pclass = tclass.encode('ascii', 'ignore')
-    data = {'classes': [pclass]}
+    data = {'classes': [escape(tclass)]}
+    data['parameters'] = {'ec2_app': escape(i.tags['App']),
+                          'ec2_env': escape(i.tags['Env']),
+                          'ec2_type': escape(i.tags['Type'])}
 
     return yaml.dump(data, default_flow_style=False, indent=10)
