17bfe0be6908d8a0673ae3ba5cb63cf23a752c6c
Jeremiah Orem <oremj@oremj.com>
2013-02-25 16:46:44 -0800

    Smaller config for puppet runs.

--- mozawsdeploy/__init__.py
+++ mozawsdeploy/__init__.py
@@ -4,7 +4,8 @@ from ConfigParser import SafeConfigParser
 from . import config
 
 
-def configure(config_file=None):
+def configure(config_file=None, access_only=False):
+    """Access only is for running the puppet ext node classifier"""
     if not config_file:
         config_file = ['/etc/awsdeploy',
                        '/etc/awsdeploy.puppet',
@@ -12,12 +13,13 @@ def configure(config_file=None):
 
     conf = SafeConfigParser()
     if conf.read(config_file):
-        config.app = conf.get('awsdeploy', 'app')
         config.aws_access_key_id = conf.get('awsdeploy',
                                             'aws_access_key_id')
         config.aws_secret_access_key = conf.get('awsdeploy',
                                                 'aws_secret_access_key')
-        config.env = conf.get('awsdeploy', 'env')
-        config.region = conf.get('awsdeploy', 'region')
-        config.puppet_ip = conf.get('awsdeploy', 'puppet_ip')
-        config.vpc_id = conf.get('awsdeploy', 'vpc_id')
+        if not access_only:
+            config.app = conf.get('awsdeploy', 'app')
+            config.env = conf.get('awsdeploy', 'env')
+            config.region = conf.get('awsdeploy', 'region')
+            config.puppet_ip = conf.get('awsdeploy', 'puppet_ip')
+            config.vpc_id = conf.get('awsdeploy', 'vpc_id')
